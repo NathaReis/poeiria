e776edb19617f7d6771972b99210a10cf6571488
NathaReis <153759333+NathaReis@users.noreply.github.com>
2024-10-17 15:32:33 -0300

    Logo
    Logo;
    Edit e Add na msm página;
    Author adicionado.

--- pages/add/script.js
+++ pages/add/script.js
@@ -1,4 +1,20 @@
+let poeiriaDados;
 const $form = document.querySelector("form");
+
+(() => {
+    poeiriaDados = JSON.parse(sessionStorage.getItem("poeiria"));
+    if(poeiriaDados) {
+        $form.author.value = poeiriaDados.author;
+        $form.title.value = poeiriaDados.title;
+        $form.text.value = poeiriaDados.lines.join("\n");
+        poeiriaDados.url ? $form.url.value = poeiriaDados.url : null;
+        $form.submit.disabled = false;
+
+        return document.querySelector("#add").remove();
+    }
+    document.querySelector("#edit").remove();
+})()
+
 
 $form.addEventListener("input", () => {
     $form.submit.disabled = $form.checkValidity() ? false : true;
@@ -8,22 +24,40 @@ $form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const poeiria = {
+        author: $form.author.value,
         title: $form.title.value,
         lines: $form.text.value.split("\n")
     }
     const regexUrl = /^\s*$/;
     !regexUrl.test($form.url.value) ? poeiria['url'] = $form.url.value : null;
 
-    fetch('https://json-server-indol-five.vercel.app/poeiria', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(poeiria)
-    })
-    .then(() => window.location = "../home/")
-    .catch((err) => {
-        console.error(err);
-        alert("Erro ao criar Poeiria!");
-    })
+    if(!poeiriaDados) {
+        fetch('https://json-server-indol-five.vercel.app/poeiria', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(poeiria)
+        })
+        .then(() => window.location = "../home/")
+        .catch((err) => {
+            console.error(err);
+            alert("Erro ao criar Poeiria!");
+        })
+    }
+    else {
+        fetch(`https://json-server-indol-five.vercel.app/poeiria/${poeiriaDados.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(poeiria)
+        })
+        .then(() => window.location = "../home/")
+        .catch((err) => {
+            console.error(err);
+            alert("Erro ao editar Poeiria!");
+        })
+    }
+
 })
